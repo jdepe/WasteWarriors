1c95a68d916ef969630db4acb521ac10f133010f
jdepe <jaden.dep@gmail.com>
2023-11-02 21:06:01 +1000

    qr code shit

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -29,7 +29,8 @@ android {
 }
 
 dependencies {
-
+    implementation("com.google.android.gms:play-services-vision:20.1.3")
+    implementation("com.google.zxing:core:3.4.1")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.9.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
--- app/src/main/java/com/example/wastewarriors/qrScanActivity.java
+++ app/src/main/java/com/example/wastewarriors/qrScanActivity.java
@@ -1,13 +1,36 @@
 package com.example.wastewarriors;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+
+import android.Manifest;
 
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.util.Log;
+import android.util.SparseArray;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
 import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.android.gms.vision.CameraSource;
+import com.google.android.gms.vision.Detector;
+import com.google.android.gms.vision.barcode.Barcode;
+import com.google.android.gms.vision.barcode.BarcodeDetector;
+
+import java.io.IOException;
 
 public class qrScanActivity extends AppCompatActivity {
     Button addCodeManuallyButton;
+    SurfaceView surfaceView;
+
+    private BarcodeDetector barcodeDetector;
+    private CameraSource cameraSource;
+    private static final int REQUEST_CAMERA_PERMISSION = 201;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,8 +43,80 @@ public class qrScanActivity extends AppCompatActivity {
             startActivity(intent);
         });
     }
-
     private void setViewIds() {
         addCodeManuallyButton = findViewById(R.id.btnAddCodeManually);
+        surfaceView = findViewById(R.id.surfaceView);
+    }
+
+    private void initialiseDetectorsAndSources() {
+        Toast.makeText(getApplicationContext(), "Scanner started", Toast.LENGTH_SHORT).show();
+        barcodeDetector = new BarcodeDetector.Builder(this).setBarcodeFormats(Barcode.ALL_FORMATS).build();
+
+        cameraSource = new CameraSource.Builder(this, barcodeDetector).setRequestedPreviewSize(1920, 1080).setAutoFocusEnabled(true).build();
+
+        surfaceView.getHolder().addCallback(new SurfaceHolder.Callback() {
+            @Override
+            public void surfaceCreated(@NonNull SurfaceHolder surfaceHolder) {
+                try {
+                    if (ActivityCompat.checkSelfPermission(qrScanActivity.this, Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED) {
+                        if (barcodeDetector.isOperational()) {
+                            cameraSource.start(surfaceView.getHolder());
+                        } else {
+                            Log.d("TAG", "Barcode detector is not operational");
+                        }
+                    } else {
+                        ActivityCompat.requestPermissions(qrScanActivity.this, new String[]{Manifest.permission.CAMERA}, REQUEST_CAMERA_PERMISSION);
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                    Log.d("TAG", "Error starting camera source: " + e.getMessage());
+                }
+            }
+
+
+            @Override
+            public void surfaceChanged(@NonNull SurfaceHolder surfaceHolder, int format, int width, int height) {
+            }
+
+            @Override
+            public void surfaceDestroyed(@NonNull SurfaceHolder surfaceHolder) {
+                cameraSource.stop();
+            }
+        });
+
+        barcodeDetector.setProcessor(new Detector.Processor<Barcode>() {
+            @Override
+            public void release() {
+                Toast.makeText(getApplicationContext(), "Scanner stopped", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void receiveDetections(@NonNull Detector.Detections<Barcode> detections) {
+                final SparseArray<Barcode> barcodes = detections.getDetectedItems();
+                if (barcodes.size() != 0) {
+                    runOnUiThread(() -> {
+                        Barcode barcode = barcodes.valueAt(0);
+                        if (barcode != null && barcode.displayValue != null) {
+                            Log.d("QRCodeScanner", "Scanned value: " + barcode.displayValue);
+                        } else {
+                            Log.d("QRCodeScanner", "Barcode is null");
+                        }
+                    });
+                }
+            }
+        });
+
+
+    }
+    @Override
+    protected void onPause() {
+        super.onPause();
+        cameraSource.release();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        initialiseDetectorsAndSources();
     }
 }
